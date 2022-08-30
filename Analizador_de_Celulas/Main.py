ccdf547d29c208cb72302b9f810be5f61840536a
kevin95all <58482652+kevin95all@users.noreply.github.com>
2022-08-29 21:32:47 -0600

    Menu y carga de archivos

--- Analizador_de_Celulas/Main.py
+++ Analizador_de_Celulas/Main.py
@@ -1,4 +1,80 @@
+from tkinter import *
+from tkinter import filedialog
+from XML import XML
+
+
 class Main:
 
     def __init__(self):
-        pass
+        self.ruta = ''
+        self.op = ''
+        self.salida = False
+        self.paciente_seleccionado = False
+        self.archivo = XML()
+
+    def menu_principal(self):
+        while not self.salida:
+            print('                                          ')
+            print('┌------------ MENU PRINCIPAL ------------┐')
+            print('|                                        |')
+            print('|        1)   Cargar archivo             |')
+            print('|        2)   Seleccionar paciente       |')
+            print('|        3)   Seleccionar celula         |')
+            print('|        4)   Analizar paciente          |')
+            print('|        5)   Generar reporte            |')
+            print('|        6)   Salir                      |')
+            print('|                                        |')
+            print('└----------------------------------------┘')
+            print('                                          ')
+
+            self.op = input('--> Ingrese una opción: ')
+
+            if self.op == '1':
+                self.cargar_archivo()
+            elif self.op == '2':
+                pass
+            elif self.op == '3':
+                pass
+            elif self.op == '4':
+                pass
+            elif self.op == '5':
+                pass
+            elif self.op == '6':
+                self.salir()
+            else:
+                print('                    ')
+                print('--> Opción no valida')
+
+    def cargar_archivo(self):
+        ventana = Tk()
+        respaldo = self.ruta
+        self.ruta = ''
+
+        self.ruta = filedialog.askopenfilename(
+            title='Buscar archivo',
+            filetypes=[
+                ('Archivos xml', '*.xml'),
+                ('Todos los archivos', '*.*')
+            ]
+        )
+        if self.ruta == '':
+            self.ruta = respaldo
+            print('                              ')
+            print('--> No se cargo ningun archivo')
+        else:
+            self.archivo.leer_xml(self.ruta)
+            print('                             ')
+            print('--> Archivo cargado con exito')
+        ventana.mainloop()
+
+    def salir(self):
+        print('                       ')
+        print('--> Programa finalizado')
+        self.ruta = ''
+        self.op = ''
+        self.paciente_seleccionado = False
+        self.salida = True
+
+
+app = Main()
+app.menu_principal()
--- Analizador_de_Celulas/XML.py
+++ Analizador_de_Celulas/XML.py
@@ -1,4 +1,7 @@
 class XML:
 
     def __init__(self):
+        self.ruta_xml = ''
+
+    def leer_xml(self, ruta):
         pass
